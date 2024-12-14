d67314bf795f0c575c2afc6c421f8943ab4d7afd
Jazper Garcia <jazpergarcia10@gmail.com>
2024-12-14 16:32:41 +0800

    added Maps API to the Landing Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ClarkNav';
+export class AppComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    display: any;
+    center: google.maps.LatLngLiteral = {
+        lat: 15.187769063648858,
+        lng: 120.55950164794922
+    };
+    zoom = 14;
+
+    /*------------------------------------------
+    --------------------------------------------
+    moveMap()
+    --------------------------------------------
+    --------------------------------------------*/
+    moveMap(event: google.maps.MapMouseEvent) {
+        if (event.latLng != null) this.center = (event.latLng.toJSON());
+    }
+
+    /*------------------------------------------
+    --------------------------------------------
+    move()
+    --------------------------------------------
+    --------------------------------------------*/
+    move(event: google.maps.MapMouseEvent) {
+        if (event.latLng != null) this.display = event.latLng.toJSON();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleMapsModule } from '@angular/google-maps'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
